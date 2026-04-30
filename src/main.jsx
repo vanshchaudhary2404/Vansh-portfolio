621f65c21f83c4734ae7279d8177537da59ada7e
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-04-30 21:42:50 +0530

    Add Vercle Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 
 const router = getRouter();
@@ -9,5 +10,6 @@ const router = getRouter();
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+     <Analytics />
   </React.StrictMode>,
 );
